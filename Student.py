9b23b22ad2d90b96d8dba78557fc4cf00c715efd
Jesper Petersen <info@ordklar.dk>
2019-10-13 00:02:37 +0200

    arbejder med student

---
+++ Student.py
@@ -0,0 +1,13 @@
+studentName = [
+    {"name": "Jasper", "studentId": 51918},
+    {"name": "Kasper", "studentId": 25222},
+    {"name": "Lars", "studentId": 43616},
+    {"name": "Sofie", "studentId": 25151},
+    {"name": "Hans", "studentId": 46322}
+]
+
+for name in studentName:
+    if name["name"] == "Sofie":
+        print("Du fandt " + name["name"])
+        break
+    print("Fik desværre navnet " + name["name"])
