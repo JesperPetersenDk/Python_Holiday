8f8c015caeb09b4305d4090d2a9ac9310361c3ec
Jesper Petersen <info@ordklar.dk>
2019-10-12 02:29:17 +0200

    add list

--- work.py
+++ work.py
@@ -1,6 +1,18 @@
+text = "LA"
 number = 5
+student_name = ["Mark", "Hans", "Kasper", "Jonas"]
 
 if number == 10:
     print("Det er 10")
 else:
-    print("Det er " + str(number))
+    if number == 5 and text == "Hello":
+        print("Det er " + str(number))
+    else:
+        print("Der er en fejl pt...")
+        
+        
+print(student_name)
+
+student_name[1] = "Louise"
+
+print(student_name)
