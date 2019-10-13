a6fe43f3fae8a3a79a0fa8613ab7bd4b2a0e08aa
Jesper Petersen <info@ordklar.dk>
2019-10-13 23:27:27 +0200

    kan nu opret file og læse fra den

--- functions.py
+++ functions.py
@@ -3,15 +3,39 @@ students = []
 def getStudents():
     student_TitleCase = []
     for student in students:
-        student_titleCase = student.title()
-    return student_titleCase
+        student_TitleCase.append(student["name"].title())
+    return student_TitleCase
 
 def printStudent():
     student_TitleCase = getStudents()
     print(student_TitleCase)
     
-def addStudent(name, studentId):
-    student = {"name": name, "studentId":studentId }
+def addStudent(name, area):
+    student = {"name": name, "area":area }
     students.append(student)
 
-addStudent("Jesper Petersen", 22511)
+def saveFile(student):
+    try:
+        f = open("student.txt", "a")
+        f.write(student + "\n")
+        f.close()
+    except Exception:
+        print("Could not save file")
+
+def readFile():
+    try:
+        f = open("student.txt", "r")
+        for student in f.readlines():
+            addStudent(student)
+        f.close()
+    except Exception:
+        print("Could not read file")
+
+readFile()
+printStudent()
+
+studentName = input("skriv student navn: ")
+studentArea = input("skriv området til student: ")
+
+addStudent(studentName, studentArea)
+saveFile(studentName)
